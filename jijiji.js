1617761d575fb3971df26a9397199b74b6cefd33
Arthwur <142269162+Arthwwr420@users.noreply.github.com>
2024-10-28 22:58:17 -0600

    jijiji.js

--- jijiji.js
+++ jijiji.js
@@ -1,7 +1,7 @@
 
 
 function kill(){
-    var link = "https://static.wikia.nocookie.net/listofdeaths/images/b/b9/Screenshot_%281949%29.png/revision/latest?cb=20200426225217";
+    var link = "ded.webp";
     document.getElementById('hoovy').src = link;
     var audio = new Audio('team-fortress-2-heavy---death-made-with-Voicemod.mp3');
     audio.play()
